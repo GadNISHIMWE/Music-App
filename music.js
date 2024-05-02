1eed867d19c8622950abc0b95a0ffa722ac3d629
GadNISHIMWE <gadnishimwe67@gmail.com>
2024-05-02 16:31:49 +0200

    Music App

---
+++ music.js
@@ -0,0 +1,13 @@
+const leftAngle = document.querySelector(".fa-solid fa-angle-left")
+const menu = document.querySelector(".fa-solid fa-bars")
+menu.onclick=tapMenu();
+function tapMenu(){
+    Const array =[
+        {
+            name:"songs",
+          
+        }
+        text.innerText="this is real place to play your songs";
+    ]
+    
+}
