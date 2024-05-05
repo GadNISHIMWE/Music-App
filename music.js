a51a7f327fcc043746ed549d7e7c056b334bc12e
GadNISHIMWE <gadnishimwe67@gmail.com>
2024-05-05 21:28:30 +0200

    changes on html

--- music.js
+++ music.js
@@ -2,12 +2,9 @@ const leftAngle = document.querySelector(".fa-solid fa-angle-left")
 const menu = document.querySelector(".fa-solid fa-bars")
 menu.onclick=tapMenu();
 function tapMenu(){
-    Const array =[
-        {
-            name:"songs",
-          
-        }
-        text.innerText="this is real place to play your songs";
-    ]
+    const array =[{
+        name: "town square",
+        "button text": ["Go to store", "Go to cave", "Fight dragon"]}
+    ];
     
 }
